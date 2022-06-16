fe58ef22655b7901d0104ddc5492e8cd8cefc29f
scuby <c7573167070@gmail.com>
2022-06-16 11:28:30 -0400

    Randomly generated computer play

--- script.js
+++ script.js
@@ -2,7 +2,27 @@
 
 // random computer move
 function computerPlay(){
-
+    const moveCode = Math.floor(Math.random() * 10);
+    let computerMove = '';
+    
+    switch(moveCode){
+        case 1:
+        case 2:
+        case 3: computerMove = "rock";
+            break;
+        case 4:
+        case 5:
+        case 6: computerMove = "paper";
+            break;
+        case 7:
+        case 8:
+        case 9: computerMove = "scissors";
+            break;
+        default: computerMove = "nuclear bomb"; // the extra special case where the game ends. forever.
+    }
+    console.log(computerMove);
+    
+    return computerMove;
 }
 
 // single round (user vs computer)
